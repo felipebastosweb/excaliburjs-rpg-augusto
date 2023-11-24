02b5f52ade8252a80760861cb59383290840e293
felipe bastos <felipebastosweb@gmail.com>
2023-11-24 19:58:45 -0300

    carregamento de arquivos de áudio do NPC

--- src/resources.ts
+++ src/resources.ts
@@ -1,10 +1,19 @@
 import { ImageFiltering, ImageSource, Loadable, Loader } from "excalibur";
 import { TiledMapResource } from '@excaliburjs/plugin-tiled';
 
+import { Sound } from 'excalibur';
+
 
 export const Resources = {
+    // Sprites
     HeroSpriteSheetPng: new ImageSource('./img/Solaria Demo Pack Update 03/Solaria Demo Pack Update 03/16x16/Sprites/Hero 01.png', false, ImageFiltering.Pixel),
-    TiledMap: new TiledMapResource('./res/first-level.tmx')
+    // Maps
+    TiledMap: new TiledMapResource('./res/first-level.tmx'),
+    
+    // Audios
+    NPCSpeech1: new Sound('./audio/npc_speech_1.mp3'),
+    NPCSpeech2: new Sound('./audio/npc_speech_2.mp3'),
+    // Adicione mais conforme necessário
 }
 
 // Change the path to be relative to the root directory for the webpack prod build
@@ -15,6 +24,7 @@ Resources.TiledMap.convertPath = (originPath: string, relativePath: string) => {
     }
     return convertPath(originPath, relativePath);
 }
+
 
 export const loader = new Loader();
 for (let resource of Object.values(Resources)) {
